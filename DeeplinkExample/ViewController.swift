1dd70670e9fb566ad670de939f38ef2c222ee2dd
Peter Combee <petercombee@gmail.com>
2021-07-19 15:50:31 +0200

    Initial Commit

---
+++ DeeplinkExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeeplinkExample
+//
+//  Created by Peter Combee on 19/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DeeplinkExampleTests/DeeplinkExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  DeeplinkExampleTests.swift
+//  DeeplinkExampleTests
+//
+//  Created by Peter Combee on 19/07/2021.
+//
+
+import XCTest
+@testable import DeeplinkExample
+
+class DeeplinkExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
